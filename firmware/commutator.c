dfaa579ae1f0fbf62f9b5a9299e7975008867d92
cjsha <cs@open-ephys.org>
2024-10-21 09:54:50 -0400

    Implement button logic
    - CAP1296 watched with pin interrupt
    - LED toggles according to button press
    - Logic for other buttons remain untested
    - Logic for UART messages is unimplemented

--- firmware/commutator.c
+++ firmware/commutator.c
@@ -59,16 +59,27 @@
 #define IS31_ADDR               0x68
 #define CAP12_ADDR              0x28
 
+// miscellaneous constants
 #define RPROG                   2490.0 // Charge current programming resistor (Ohms)
 #define CODE_TO_AMPS            (3.3 / 4096 * 1000.0 / RPROG) // 12-bit conversion
 
 // Power options
 
+volatile uint8_t alert_flag = 0;
+
 // Controller state
 struct Context {
-    int led_on; // Using a bool results in extemely bizarre behavior
-    int commutator_en;  // Using a bool results in extemely bizarre behavior
-} ctx;
+    uint8_t led; 
+    uint8_t enable;
+    uint8_t speed;
+    uint8_t accel;
+    uint8_t target;
+};
+
+void cap1298_alert_callback()
+{
+    alert_flag = 1;
+}
 
 void setup_io()
 {
@@ -87,9 +98,9 @@ void setup_io()
     gpio_init(IS31_SDB);
     gpio_set_dir(IS31_SDB, GPIO_OUT);
 
-    gpio_init(CAP12_ALERT);
-    gpio_set_dir(CAP12_ALERT, GPIO_IN);
-    gpio_pull_up(CAP12_ALERT);
+    // gpio_init(CAP12_ALERT);
+    // gpio_set_dir(CAP12_ALERT, GPIO_IN);
+    gpio_set_irq_enabled_with_callback(CAP12_ALERT, GPIO_IRQ_EDGE_FALL, true, &cap1298_alert_callback);
 
     i2c_init(I2C_PORT, 400000);
     gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
@@ -107,7 +118,7 @@ void setup_io()
     // gpio_set_dir(TMC2130_SCLK, GPIO_OUT);
 }
 
-inline float charge_current()
+float charge_current()
 {
     uint16_t result = adc_read();
     return result * CODE_TO_AMPS;
@@ -122,32 +133,29 @@ void set_rgb_color(uint8_t r, uint8_t g, uint8_t b)
     i2c_write_blocking(I2C_PORT, IS31_ADDR, pwm, 2, false);
 }
 
-void update_rgb()
+void update_rgb(struct Context context)
 {
-    if (!ctx.led_on) {
+    if (!context.led) 
+    {
         set_rgb_color(0x00, 0x00, 0x00);
-        return;
     }
-
-    if (!ctx.commutator_en) {
+    else if (!context.enable) 
+    {
         set_rgb_color(255, 0, 0);
-        return;
     }
-
-    set_rgb_color(1, 20, 7);
+    else
+    {
+        set_rgb_color(1, 20, 7);
+    }
+    return;
 }
 
 void setup_rgb()
 {
     gpio_put(IS31_SDB, 1);
-    // Set max current to 5 mA
-    const uint8_t max_curr[2] = {0x03, 0x08};
+    const uint8_t max_curr[2] = {0x03, 0x08}; // Set max current to 5 mA
     i2c_write_blocking(I2C_PORT, IS31_ADDR, max_curr, 2, false);
-
-    update_rgb();
-
-    // Enable current driver
-    const uint8_t en_curr[2]  = {0x00, 0x20};
+    const uint8_t en_curr[2]  = {0x00, 0x20}; // Enable current driver
     i2c_write_blocking(I2C_PORT, IS31_ADDR, en_curr, 2, false);
 }
 
@@ -165,89 +173,121 @@ void setup_power() // Stabilize charge current and breath LED in meantime
 
 void setup_touch()
 {
+    const uint8_t cap1298_button_interrupts[2] = {0x27, 0x0f};
+    i2c_write_blocking(I2C_PORT, CAP12_ADDR, cap1298_button_interrupts, 2, false);
+    const uint8_t cap1298_button_enables[2] = {0x21, 0x0f};
+    i2c_write_blocking(I2C_PORT, CAP12_ADDR, cap1298_button_enables, 2, false);
+    const uint8_t cap1298_button_autorepeat[2] = {0x28, 0x00};
+    i2c_write_blocking(I2C_PORT, CAP12_ADDR, cap1298_button_autorepeat, 2, false);
+    const uint8_t cap1298_sensitivity[2] = {0x1f, 0x5f};
+    i2c_write_blocking(I2C_PORT, CAP12_ADDR, cap1298_sensitivity, 2, false);
+    const uint8_t cap1298_calibrate[2] = {0x26, 0x0f};
+    i2c_write_blocking(I2C_PORT, CAP12_ADDR, cap1298_calibrate, 2, false);
+    const uint8_t cap1298_config2[2] = {0x44, 0x41};
+    i2c_write_blocking(I2C_PORT, CAP12_ADDR, cap1298_config2, 2, false);
+}
+
+void print_diagnostics()
+{
+    uint8_t buffer[1] = {0};
+    sleep_ms(15);
+    const uint8_t cap1298_device_id_register = 0xfd;
+    i2c_write_blocking(I2C_PORT, CAP12_ADDR, &cap1298_device_id_register, 1, true);
+    i2c_read_blocking(I2C_PORT, CAP12_ADDR, buffer, 1, false);
+    sleep_ms(500);
+    printf("device id: %02hhx\n", buffer[0]);
+    const uint8_t cap1298_control_register = 0x00;
+    i2c_write_blocking(I2C_PORT, CAP12_ADDR, &cap1298_control_register, 1, true);
+    i2c_read_blocking(I2C_PORT, CAP12_ADDR, buffer, 1, false);
+    printf("control: %02hhx\n", buffer[0]);
+    const uint8_t cap1298_status_register = 0x02;
+    i2c_write_blocking(I2C_PORT, CAP12_ADDR, &cap1298_status_register, 1, true);
+    i2c_read_blocking(I2C_PORT, CAP12_ADDR, buffer, 1, false);
+    printf("status: %02hhx\n", buffer[0]);
+    const uint8_t cap1298_calibration_status_register = 0x26;
+    i2c_write_blocking(I2C_PORT, CAP12_ADDR, &cap1298_calibration_status_register, 1, true);
+    i2c_read_blocking(I2C_PORT, CAP12_ADDR, buffer, 1, false);
+    printf("calibration: %02hhx\n", buffer[0]);
+    const uint8_t cap1298_noise_register = 0x0a;
+    i2c_write_blocking(I2C_PORT, CAP12_ADDR, &cap1298_noise_register, 1, true);
+    i2c_read_blocking(I2C_PORT, CAP12_ADDR, buffer, 1, false);
+    printf("noise: %02hhx\n", buffer[0]);
+    const uint8_t cap1298_cal1_register = 0xb1;
+    i2c_write_blocking(I2C_PORT, CAP12_ADDR, &cap1298_cal1_register, 1, true);
+    i2c_read_blocking(I2C_PORT, CAP12_ADDR, buffer, 1, false);
+    printf("cal1: %02hhx\n", buffer[0]);
+    const uint8_t cap1298_cal2_register = 0xb2;
+    i2c_write_blocking(I2C_PORT, CAP12_ADDR, &cap1298_cal2_register, 1, true);
+    i2c_read_blocking(I2C_PORT, CAP12_ADDR, buffer, 1, false);
+    printf("cal2: %02hhx\n", buffer[0]);
+    const uint8_t cap1298_cal3_register = 0xb3;
+    i2c_write_blocking(I2C_PORT, CAP12_ADDR, &cap1298_cal3_register, 1, true);
+    i2c_read_blocking(I2C_PORT, CAP12_ADDR, buffer, 1, false);
+    printf("cal3: %02hhx\n", buffer[0]);
+    const uint8_t cap1298_cal4_register = 0xb4;
+    i2c_write_blocking(I2C_PORT, CAP12_ADDR, &cap1298_cal4_register, 1, true);
+    i2c_read_blocking(I2C_PORT, CAP12_ADDR, buffer, 1, false);
+    printf("cal4: %02hhx\n", buffer[0]);
+    const uint8_t cap1298_lsb1_register = 0xb9;
+    i2c_write_blocking(I2C_PORT, CAP12_ADDR, &cap1298_lsb1_register, 1, true);
+    i2c_read_blocking(I2C_PORT, CAP12_ADDR, buffer, 1, false);
+    printf("lsb1: %02hhx\n", buffer[0]);
+}
+
+void turn_motor(uint8_t direction, struct Context context)
+{
 
 }
 
 int main() 
 {
+    struct Context ctx;
+    ctx.led = 1;
+    uint8_t turn_direction;
+    uint8_t turn_flag;
     setup_io();
+    update_rgb(ctx);
     setup_rgb();
     setup_power();
     setup_touch();
-    const uint8_t cap1298_control_register = 0x00;
-    const uint8_t cap1298_status_register = 0x02;
-    const uint8_t cap1298_button_state_register = 0x03;
-    const uint8_t cap1298_noise_register = 0x0a;
-    const uint8_t cap1298_calibration_status_register = 0x26;
-    const uint8_t cap1298_device_id_register = 0xfd;
-    const uint8_t cap1298_cal1_register = 0xb1;
-    const uint8_t cap1298_cal2_register = 0xb2;
-    const uint8_t cap1298_cal3_register = 0xb3;
-    const uint8_t cap1298_cal4_register = 0xb4;
-    const uint8_t cap1298_cal5_register = 0xb5;
-    const uint8_t cap1298_cal6_register = 0xb6;
+    #ifdef DEBUG
+        print_diagnostics();
+    #endif
     uint8_t buffer[1] = {0};
     const uint8_t cap1298_clear_alert[2] = {0x00, 0x00};
-    const uint8_t cap1298_sensitivity[2] = {0x1f, 0x5f};
-    const uint8_t cap1298_button_enables[2] = {0x21, 0x0f};
-    const uint8_t cap1298_calibrate[2] = {0x26, 0x0f};
-    const uint8_t cap1298_button_interrupts[2] = {0x27, 0x0f};
-    const uint8_t cap1298_button_autorepeat[2] = {0x28, 0x00};
-    uint16_t button_counter = 0;
-    i2c_write_blocking(I2C_PORT, CAP12_ADDR, cap1298_button_interrupts, 2, true);
-    i2c_write_blocking(I2C_PORT, CAP12_ADDR, cap1298_button_enables, 2, true);
-    i2c_write_blocking(I2C_PORT, CAP12_ADDR, cap1298_button_autorepeat, 2, true);
-    i2c_write_blocking(I2C_PORT, CAP12_ADDR, cap1298_sensitivity, 2, true);
-    i2c_write_blocking(I2C_PORT, CAP12_ADDR, cap1298_calibrate, 2, false);
-    i2c_write_blocking(I2C_PORT, CAP12_ADDR, &cap1298_device_id_register, 1, true);
-    i2c_read_blocking(I2C_PORT, CAP12_ADDR, buffer, 1, false);
-    sleep_ms(500);
-    printf("device id: %02hhx\n", buffer[0]);
-    sleep_ms(2000);
-    i2c_write_blocking(I2C_PORT, CAP12_ADDR, &cap1298_control_register, 1, true);
-    i2c_read_blocking(I2C_PORT, CAP12_ADDR, buffer, 1, false);
-    printf("control: %02hhx\n", buffer[0]);
-    i2c_write_blocking(I2C_PORT, CAP12_ADDR, cap1298_clear_alert, 2, false);
-    i2c_write_blocking(I2C_PORT, CAP12_ADDR, &cap1298_control_register, 1, true);
-    i2c_read_blocking(I2C_PORT, CAP12_ADDR, buffer, 1, false);
-    printf("control: %02hhx\n", buffer[0]);
-    i2c_write_blocking(I2C_PORT, CAP12_ADDR, &cap1298_status_register, 1, true);
-    i2c_read_blocking(I2C_PORT, CAP12_ADDR, buffer, 1, false);
-    printf("status: %02hhx\n", buffer[0]);
-    i2c_write_blocking(I2C_PORT, CAP12_ADDR, &cap1298_calibration_status_register, 1, true);
-    i2c_read_blocking(I2C_PORT, CAP12_ADDR, buffer, 1, false);
-    printf("calibration: %02hhx\n", buffer[0]);
-    i2c_write_blocking(I2C_PORT, CAP12_ADDR, &cap1298_noise_register, 1, true);
-    i2c_read_blocking(I2C_PORT, CAP12_ADDR, buffer, 1, false);
-    printf("noise: %02hhx\n", buffer[0]);
-    i2c_write_blocking(I2C_PORT, CAP12_ADDR, &cap1298_cal1_register, 1, true);
-    i2c_read_blocking(I2C_PORT, CAP12_ADDR, buffer, 1, false);
-    printf("cal1: %02hhx\n", buffer[0]);
-    i2c_write_blocking(I2C_PORT, CAP12_ADDR, &cap1298_cal2_register, 1, true);
-    i2c_read_blocking(I2C_PORT, CAP12_ADDR, buffer, 1, false);
-    printf("cal2: %02hhx\n", buffer[0]);
-    i2c_write_blocking(I2C_PORT, CAP12_ADDR, &cap1298_cal3_register, 1, true);
-    i2c_read_blocking(I2C_PORT, CAP12_ADDR, buffer, 1, false);
-    printf("cal3: %02hhx\n", buffer[0]);
-    i2c_write_blocking(I2C_PORT, CAP12_ADDR, &cap1298_cal4_register, 1, true);
-    i2c_read_blocking(I2C_PORT, CAP12_ADDR, buffer, 1, false);
-    printf("cal4: %02hhx\n", buffer[0]);
-    i2c_write_blocking(I2C_PORT, CAP12_ADDR, &cap1298_cal5_register, 1, true);
-    i2c_read_blocking(I2C_PORT, CAP12_ADDR, buffer, 1, false);
-    printf("cal5: %02hhx\n", buffer[0]);
-    i2c_write_blocking(I2C_PORT, CAP12_ADDR, &cap1298_cal6_register, 1, true);
-    i2c_read_blocking(I2C_PORT, CAP12_ADDR, buffer, 1, false);
-    printf("cal6: %02hhx\n", buffer[0]);
+    const uint8_t cap1298_button_state_register = 0x03;
     while (true) 
-    {
-        if (gpio_get(CAP12_ALERT) == 0)
+    {   
+        if (alert_flag == 1)
         {
             i2c_write_blocking(I2C_PORT, CAP12_ADDR, &cap1298_button_state_register, 1, true);
             i2c_read_blocking(I2C_PORT, CAP12_ADDR, buffer, 1, false);
             i2c_write_blocking(I2C_PORT, CAP12_ADDR, cap1298_clear_alert, 2, false);
-            printf("button touch: %02hhx, %u\n", buffer[0], ++button_counter);
+            alert_flag = 0;
+            switch (buffer[0]){
+                case 0x04:
+                    ctx.led = !ctx.led;
+                    break;
+                case 0x02:
+                    ctx.enable = !ctx.enable;
+                    break;
+                case 0x01:
+                    turn_flag = 1;
+                    turn_direction = 1;
+                    break;
+                case 0x08:
+                    turn_flag = 1;
+                    turn_direction = 0;
+                    break;
+            }
+            update_rgb(ctx);
+            if (turn_flag)
+            {
+                turn_motor(turn_direction, ctx);
+                turn_flag = 0;
+            }
+            // printf("button: %02hhx, %u\n", buffer[0], ++button_counter);
         }
     }
-
     return 0;
 }
