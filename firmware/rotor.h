528de8c7b0d345d94a7fac9b4d86daffcc55c6ad
cjsha <36574350+cjsha@users.noreply.github.com>
2026-04-02 14:28:22 -0400

    Decelerate button presses, decrease stepper current draw (#43)
    * soft-stop instead of hard-stop button releases
    
    * increase soft-stop deceleration
    
    * Decrease stepper motor/driver current draw
    
    * Set Vsense to 1 and Increase current scaling
    
    * Make turning quieter
    
    - Increase microstepping
    - Document all TM2130 settings in comments
    
    ---------
    
    Co-authored-by: jonnew <jpn@open-ephys.org>

--- firmware/rotor.h
+++ firmware/rotor.h
@@ -6,13 +6,13 @@
 
 // Stepper and driver parameters
 #define DETENTS 200
-#define USTEPS_PER_STEP 8
+#define USTEPS_PER_STEP 64
 #define USTEPS_PER_REV (DETENTS * USTEPS_PER_STEP)
 #define MAX_TURNS(gear_ratio) (INT_MAX / USTEPS_PER_REV / gear_ratio)
 
 // Turn speed and acceleration
 #define SPEED_RPM 100
-#define ACCEL_RPMM 150
+#define ACCEL_RPMM 100
 #define MAX_SPEED_SPS(gear_ratio) (USTEPS_PER_REV * gear_ratio * SPEED_RPM / 60.0L)
 #define MAX_ACCEL_SPSS(gear_ratio) (USTEPS_PER_REV * gear_ratio * ACCEL_RPMM / 60.0L)
 
